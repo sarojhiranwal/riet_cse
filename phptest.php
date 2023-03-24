f32a2b4064775ae1f3fa761a75ebba278b360e1d
sarojhiranwal <ersarojhirnanwal@gmail.com>
2023-03-24 19:22:56 +1100

    error message change

--- phptest.php
+++ phptest.php
@@ -8,7 +8,7 @@ $dbname = "phptest";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  die("Connection failed because of: " . $conn->connect_error);
 }
 
 ?>
